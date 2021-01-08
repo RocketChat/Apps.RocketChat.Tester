7394c0335cf3e5b7ee3c8a9008451eddc2e3f699
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-01-08 14:49:33 -0300

    Sends a message with a custom field and an attachment using the messageBuilder

--- slashcommands/ExtendSlashcommand.ts
+++ slashcommands/ExtendSlashcommand.ts
@@ -1,7 +1,6 @@
 import { IHttp, IModify, IPersistence, IRead, IMessageExtender } from '@rocket.chat/apps-engine/definition/accessors';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
 import { ImageAttachment } from '../ImageAttachment';
-import { Message } from '../Message'
 
 export class ExtendMessageCommand implements ISlashCommand{
     public command = 'extend-message';
@@ -10,21 +9,23 @@ export class ExtendMessageCommand implements ISlashCommand{
     public providesPreview = false;
 
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
-        var messageId = await this.sendMessage(context, modify, 'Sending a message!');
+        await this.sendMessage(context, modify, 'Sending one message!');
     }
 
     private async sendMessage(context: SlashCommandContext, modify: IModify, message: string): Promise<string> {
         const messageStructure = modify.getCreator().startMessage();
         const sender = context.getSender();
         const room = context.getRoom();
-        const value = 1;
+        const value = 2;
         const img = new ImageAttachment('https://open.rocket.chat/images/logo/logo.svg');
-
-        const msg = new Message(sender, room, message);
-        messageStructure.setData(msg);
-
-        msg.addCustomField('key', value);
-        msg.addAttachment(img);
+        
+        messageStructure
+            .setSender(sender)
+            .setRoom(room)
+            .setText(message)
+            .addAttachment(img);
+        
+        messageStructure.addCustomField('key', value);
 
         return (await modify.getCreator().finish(messageStructure));
     }
