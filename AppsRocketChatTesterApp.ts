544d0a5df8108b44402aa4ededf7e1b797fed235
murtaza98 <murtaza.patrawala@rocket.chat>
2022-04-07 10:48:44 +0530

    Refactoring endpoint to create rooms

--- AppsRocketChatTesterApp.ts
+++ AppsRocketChatTesterApp.ts
@@ -2,6 +2,7 @@ import { IAppAccessors, IConfigurationExtend, ILogger } from '@rocket.chat/apps-
 import { ApiSecurity, ApiVisibility } from '@rocket.chat/apps-engine/definition/api';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { CreateOrGetOmnichannelRoomForVisitorEndpoint } from './endpoints/CreateOrGetOmnichannelRoomForVisitor';
 import { CreateVisitorEndpoint } from './endpoints/CreateVisitor';
 import { QueryVisitorEndpoint } from './endpoints/QueryVisitor';
 import { SendMessageAsAppUserEndpoint } from './endpoints/SendMessageAsAppUser';
@@ -23,6 +24,7 @@ export class RocketChatTester extends App {
                 new SendMessageAsUserEndpoint(this),
                 new QueryVisitorEndpoint(this),
                 new CreateVisitorEndpoint(this),
+                new CreateOrGetOmnichannelRoomForVisitorEndpoint(this),
             ],
         });
 
--- endpoints/CreateOrGetOmnichannelRoomForVisitor.ts
+++ endpoints/CreateOrGetOmnichannelRoomForVisitor.ts
@@ -3,8 +3,8 @@ import { ApiEndpoint, IApiEndpointInfo, IApiRequest, IApiResponse } from '@rocke
 import { IVisitor } from '@rocket.chat/apps-engine/definition/livechat';
 import { safeJsonParse } from '../lib/safeJsonParse';
 
-export class CreateVisitorEndpoint extends ApiEndpoint {
-    public path = 'get-omnichannel-room-for-visitor';
+export class CreateOrGetOmnichannelRoomForVisitorEndpoint extends ApiEndpoint {
+    public path = 'create-or-get-omnichannel-room-for-visitor';
 
     // tslint:disable-next-line: max-line-length
     public async post(request: IApiRequest, endpoint: IApiEndpointInfo, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<IApiResponse> {
