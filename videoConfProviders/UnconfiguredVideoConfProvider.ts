6dd3eb554b031b4d4911a6c2245c0c57ebb6f647
Pierre Lehnen <55164754+pierre-lehnen-rc@users.noreply.github.com>
2022-06-29 09:51:28 -0300

    Include video conference providers on TesterApp (#5)
    * [NEW] Video Conference Providers
    
    * fake engine number
    
    * changed test app capabilities
    
    * apps-engine version

---
+++ videoConfProviders/UnconfiguredVideoConfProvider.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import type { IVideoConferenceUser } from '@rocket.chat/apps-engine/definition/videoConferences/IVideoConferenceUser';
+import type {
+    IVideoConfProvider,
+    VideoConfData,
+    VideoConfDataExtended,
+    IVideoConferenceOptions,
+} from '@rocket.chat/apps-engine/definition/videoConfProviders';
+
+export class UnconfiguredVideoConfProvider implements IVideoConfProvider {
+    public name = 'unconfigured'
+
+    public capabilities = {
+        mic: false,
+        cam: false,
+        title: false,
+    }
+
+    public async isFullyConfigured(): Promise<boolean> {
+        return false;
+    }
+
+    public async generateUrl(call: VideoConfData): Promise<string> {
+        return `unconfigured/${call._id}`;
+    }
+
+    public async customizeUrl(call: VideoConfDataExtended, user: IVideoConferenceUser, options: IVideoConferenceOptions): Promise<string> {
+        return call.url;
+    }
+}
